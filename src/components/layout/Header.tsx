8d7aa59f7f99bbcea826d67340b9070dda3b07b5
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-06 23:17:18 +0530

    Updated new styles & added components

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,19 @@
+import FavoriteBtn from "./FavoriteBtn";
 import HeaderTitle from "./HeaderTitle";
+import ThemeMode from "./ThemeMode";
 
 export default function Header() {
     return (
         <>
-            <header>
-                <HeaderTitle title='JokeBuzz' />
+            <header className="w-full border-b">
+                <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
+                    <HeaderTitle title="JokeBuzz" />
+
+                    <div className="flex items-center gap-3">
+                        <ThemeMode />
+                        <FavoriteBtn />
+                    </div>
+                </div>
             </header>
         </>
     )
