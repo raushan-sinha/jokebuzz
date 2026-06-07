f5383878efe7cbe4b52e431464da7bebce070d3f
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-07 14:32:58 +0530

    Added logic for changing Icon & added style

--- src/components/layout/ThemeMode.tsx
+++ src/components/layout/ThemeMode.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { LuMoon } from "react-icons/lu";
 import { MdOutlineWbSunny } from "react-icons/md";
 
 export default function ThemeMode() {
+    const [isDark, setIsDark] = useState<boolean>(false);
+
     return (
-        <button aria-label="theme-toggle">
-            <LuMoon fontSize={25} />
-            <MdOutlineWbSunny fontSize={25} />
+        <button aria-label="theme-toggle" className="cursor-pointer" onClick={() => setIsDark(!isDark)}>
+            {isDark ? (
+                <LuMoon fontSize={25} />
+            ) : (
+                <MdOutlineWbSunny fontSize={25} />
+            )}
         </button>
     )
 }
