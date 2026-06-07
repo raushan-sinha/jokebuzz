120bda0cd982fd677d2c48783cd1f774ca8ed5db
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-07 14:37:02 +0530

    Added a smile icon

--- src/components/layout/HeaderTitle.tsx
+++ src/components/layout/HeaderTitle.tsx
@@ -1,9 +1,14 @@
+import { FaSmile } from "react-icons/fa";
+
 interface TitleProps {
     title: string
 }
 
 export default function HeaderTitle({ title }: TitleProps) {
     return (
-        <h1 className="text-red-600 text-2xl bold font-mono">{title}</h1>
+        <div className="flex items-center gap-1">
+            <FaSmile fontSize={25} />
+            <h1 className="text-red-600 text-2xl bold font-mono">{title}</h1>
+        </div>
     )
 }
