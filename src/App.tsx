30da82fd38fb37c7cbb264fc4a52448064b49fe6
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-07 21:08:52 +0530

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
                 <Header />
             </nav>
 
-            <main>
-                
+            <main className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
+                <h1 className="p-25">Hello</h1>
             </main>
         </>
     )
