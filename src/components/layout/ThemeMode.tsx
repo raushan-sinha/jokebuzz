1ab5888c678836268ab7767de6f56a83e4bbd495
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-07 14:27:50 +0530

    Adedd an Icon - Sun

--- src/components/layout/ThemeMode.tsx
+++ src/components/layout/ThemeMode.tsx
@@ -1,9 +1,11 @@
 import { LuMoon } from "react-icons/lu";
+import { MdOutlineWbSunny } from "react-icons/md";
 
 export default function ThemeMode() {
     return (
-        <button>
+        <button aria-label="theme-toggle">
             <LuMoon fontSize={25} />
+            <MdOutlineWbSunny fontSize={25} />
         </button>
     )
 }
