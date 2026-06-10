c7543c3fbbc2febe284b8626033fec6b85af2b91
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-10 21:14:37 +0530

    Added logic for Blog page navigation

--- src/components/layout/BlogBtn.tsx
+++ src/components/layout/BlogBtn.tsx
@@ -1,9 +1,17 @@
 import { FaBlog } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 export default function FavoriteBtn() {
+    const navigate = useNavigate();
+
+    const blogPage = () => {
+        navigate('/blog')
+    }
+
     return (
         <button
             className="flex items-center gap-2 rounded-full border px-4 py-2 text-sm font-medium transition-all hover:scale-105 hover:shadow-md cursor-pointer"
+            onClick={blogPage}
         >
             <span>
                 <FaBlog color="red" />
