46a1d7d9f3e063c64b51b0afb43563ca2d939fae
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-10 21:16:49 +0530

    Added react-router-dom for navigate each page and added new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Header } from "./components/layout"
-import { Home } from "./pages"
+import { Blog, Home } from "./pages"
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <nav>
                 <Header />
             </nav>
 
+
             <main className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white p-10">
-                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                </Routes>
             </main>
-        </>
+        </BrowserRouter>
     )
 }
 
